4995c4a92c9ca1c676026dfeda2921a74956a4dd
LuisAraya07 <andresaraya07@hotmail.com>
2019-12-24 17:28:02 -0600

    GruposController para móvil prueba

---
+++ SAEE_WEB/Controllers/gruposController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SAEE_WEB.Models;
+
+namespace SAEE_WEB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Gruposcontroller : ControllerBase
+    {
+
+
+        private readonly BDSAEEContext _context;
+
+        public Gruposcontroller(BDSAEEContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Grupos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Grupos>>> GetGrupos()
+        {
+            return await _context.Grupos.ToListAsync();
+        }
+
+        // GET: api/Grupos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Grupos>> GetGrupos(int id)
+        {
+            var grupos = await _context.Grupos.FindAsync(id);
+
+            if (grupos == null)
+            {
+                return NotFound();
+            }
+
+            return grupos;
+        }
+
+        // PUT: api/Grupos/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGrupos(int id, Grupos grupos)
+        {
+            if (id != grupos.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(grupos).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GruposExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Grupos
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Grupos>> PostGrupos(Grupos grupos)
+        {
+            _context.Grupos.Add(grupos);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProfesores", new { id = grupos.Id }, grupos);
+        }
+
+        // DELETE: api/Grupos/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Grupos>> DeleteGrupos(int id)
+        {
+            var grupos = await _context.Grupos.FindAsync(id);
+            if (grupos == null)
+            {
+                return NotFound();
+            }
+
+            _context.Grupos.Remove(grupos);
+            await _context.SaveChangesAsync();
+
+            return grupos;
+        }
+
+        private bool GruposExists(int id)
+        {
+            return _context.Grupos.Any(e => e.Id == id);
+        }
+    }
+}
