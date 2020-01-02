0a2e574262c05debae8387257dfac82cbc67beda
Heiner Salvatierra Ramírez <Heiner007@outlook.es>
2020-01-02 15:47:12 -0600

    Implementación de CursosController

---
+++ SAEE_WEB/Controllers/CursosController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SAEE_WEB.Models;
+
+namespace SAEE_WEB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursosController : ControllerBase
+    {
+        private readonly BDSAEEContext _context;
+
+        public CursosController(BDSAEEContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cursos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cursos>>> GetCursos()
+        {
+            return await _context.Cursos.Include(curso => curso.CursosGrupos)
+                .ThenInclude(cursoGrupo => cursoGrupo.IdGrupoNavigation).ToListAsync();
+        }
+
+        // GET: api/Cursos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cursos>> GetCursos(int id)
+        {
+            var cursos = await _context.Cursos.FindAsync(id);
+
+            if (cursos == null)
+            {
+                return NotFound();
+            }
+
+            return cursos;
+        }
+
+        // PUT: api/Cursos/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCursos(int id, Cursos cursos)
+        {
+            if (id != cursos.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cursos).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CursosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cursos
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Cursos>> PostCursos(Cursos cursos)
+        {
+            _context.Cursos.Add(cursos);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCursos", new { id = cursos.Id }, cursos);
+        }
+
+        // DELETE: api/Cursos/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Cursos>> DeleteCursos(int id)
+        {
+            var cursos = await _context.Cursos.Include(curso => curso.CursosGrupos)
+                .ThenInclude(cursoGrupo => cursoGrupo.IdGrupoNavigation)
+                .Where(curso => curso.Id == id).FirstOrDefaultAsync();
+            if (cursos == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cursos.Remove(cursos);
+            await _context.SaveChangesAsync();
+
+            return cursos;
+        }
+
+        private bool CursosExists(int id)
+        {
+            return _context.Cursos.Any(e => e.Id == id);
+        }
+    }
+}
