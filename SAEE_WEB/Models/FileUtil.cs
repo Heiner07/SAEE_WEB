d8b4604d3a8618a226c42d693ff9b1ddcd520059
TheCrisFX <cris95vg@outlook.com>
2020-01-20 17:30:37 -0600

    Agregado generacion de pdf

---
+++ SAEE_WEB/Models/FileUtil.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace SAEE_WEB.Models
+{
+    public static class FileUtil
+    {
+        public static ValueTask<object> SaveAs(this IJSRuntime js, string filename, byte[] data)
+           => js.InvokeAsync<object>(
+               "saveAsFile",
+               filename,
+               Convert.ToBase64String(data));
+    }
+}
